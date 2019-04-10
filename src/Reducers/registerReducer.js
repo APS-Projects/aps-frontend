ff247fba16732f7f66dc73de1d849c2ec9ca1121
awieckert <wiec1369@gmail.com>
2019-04-09 21:00:21 -0500

    STUFF

--- src/Reducers/registerReducer.js
+++ src/Reducers/registerReducer.js
@@ -7,20 +7,27 @@ const intialUserState = {
         email: "wiec1369@gmail.com",
         firstName: "Adam",
         lastName: "Wieckert",
-        password: "adam"
-    },
-    accessToken: "dkdfYncHDKAMCncks123"
-};                                         
+        password: "adam",
+        accessToken: "dkdfYncHDKAMCncks123"
+    }
+};
+
+function updateEmailReducer(state, action) {
+    switch(action.type) {
+        case "UPDATE_USER_EMAIL":
+            return action.userEmail;
+        default:
+            return state;    
+    }
+}
 
 function registerReducer(state = intialUserState, action) {
     switch (action.type) {
         case "UPDATE_USER_EMAIL":
-            return Object.assign({}, ...state, {userInfo: { email: action.userInfo }});
+            return Object.assign({}, ...state, { email: updateEmailReducer(state.userInfo.email, action)});
         default:
             return state;
     }
-
-    return state;
 }
 
 export default registerReducer;
